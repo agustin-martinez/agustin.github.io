f198c2d3e0c2be53cb9554a20b64d70761bfde71
Agustin Martinez <agustin.martinez@hotmail.com>
2019-11-29 14:14:12 +0100

    new-mall

---
+++ js/script.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", () => {
+    var burgerBtn = document.getElementById("burgerBtn");
+    var mobile = document.getElementById("mobile");
+  
+    burgerBtn.addEventListener(
+      "click",
+      function() {
+        mobile.classList.toggle("navigation");
+      },
+      false
+    );
+  });
+  
